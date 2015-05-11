7492d52dd84e987092eb93a0630424fa760698ec
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-11 18:01:31 +0530

    Add authentication functionality - non UI auth flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-class Sessions < ActiveRecord::Base
+class User < ActiveRecord::Base
+
 	def self.find_or_create_by_auth(auth_data)
 		user = User.find_or_create_by(provider: auth_data[:provider], uid: auth_data[:uid])
 		if user.name != auth_data[:info][:name]
@@ -7,4 +8,5 @@ class Sessions < ActiveRecord::Base
 		end
 		user
 	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :people
   get '/auth/:provider/callback', to: 'sessions#create'
   resources :sessions, :only => [:create]
-  
+  root 'companies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
